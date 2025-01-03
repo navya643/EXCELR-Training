8741e27c8a73bf51c1a8d383d1a51371bf345983
navya643 <navya.telukondi643@gmail.com>
2025-01-03 10:10:07 +0530

    Add files via upload

---
+++ Demo6.java
@@ -0,0 +1,11 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username, password;
+        username = "Navya";
+        password = "navya@5522";
+        while((username == "Navya") && (password == "navya@5522")) {
+            System.out.println("Welcome navya");
+            break;
+        }
+    }
+}
